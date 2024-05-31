fd1dd10575e91c93fa952804eea876e345a43a1b
Gfgo <gfgo033@hotmail.com>
2024-05-31 15:07:10 -0500

    Update Mordaza.ino
    31/05/2024
    velocidad lineal +corriente+oled ok

--- Mordaza/Mordaza.ino
+++ Mordaza/Mordaza.ino
@@ -1,188 +1,12 @@
-//#include <Arduino.h>
-//#include <U8g2lib.h>
-//#include <Wire.h>
-//
-//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 22, /* data=*/ 21, /* reset=*/ U8X8_PIN_NONE);
-//
-//void setup(void) {
-//
-//  u8g2.begin();
-//}
-//
-//void drawLogo(void){
-//    u8g2.setFontMode(1);  // Transparent
-//    u8g2.setFontDirection(0);
-//    u8g2.setFont(u8g2_font_inb16_mf);
-//    u8g2.drawStr(2, 22, "U");
-//    u8g2.setFontDirection(1);
-//    u8g2.setFont(u8g2_font_inb19_mn);
-//    u8g2.drawStr(14,8,"8");
-//    u8g2.setFontDirection(0);
-//    u8g2.setFont(u8g2_font_inb16_mf);
-//    u8g2.drawStr(36,22,"g");
-//    u8g2.drawStr(48,22,"\xb2");
-//}
-//
-//void loop(void) {
-//  u8g2.clearBuffer();
-//  drawLogo();
-//  u8g2.sendBuffer();
-//  delay(1000);
-//}
-////------------------------------------------------------------------------------------//27/05/2024
-
-#include <Arduino.h>
+//////////------------------------------------------------- velocidad lineal 31/05/2024+corriente+oled ok
 #include <U8g2lib.h>
-#include <Wire.h>
-
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 22, /* data=*/ 21, /* reset=*/ U8X8_PIN_NONE);
-
-//float sensibilidad=0.185;   //sensibilidad para sensor 5 Amperes
-float sensibilidad=0.100;   //sensibilidad para sensor 20 Amperes
-//float sensibilidad=0.66;    //sensibilidad para sensor 30 Amperes
-
-//float sensibilidad=0.11;      //sensibilidad en V/A para nuestro sensor 0.194 sensor de 20A
-float offset=0.092;           // Equivale a la amplitud del ruido
-float iPin = 34;        //Sensor IO34
-//float seni=0;
-float Ip =0;                  //corriente pico
-float Irms =0;               //Intensidad RMS = Ipico/(2^1/2)
-float P =0;                 // P=IV watts
-
-void setup() {
-  Serial.begin(9600);
-  u8g2.begin();
-  u8g2.clearDisplay();
-}
-
-void loop() {
-  
-  float Ip =get_corriente();//obtenemos la corriente pico
-  float Irms =Ip*0.707; //Intensidad RMS = Ipico/(2^1/2)
-  float P =Irms*220.0; // P=IV watts
-  Serial.print("Ip: ");
-  Serial.print(Ip,3);
-  Serial.print("A , I-Rms: ");
-  Serial.print(Irms,3);
-  Serial.print("A, Potencia: ");
-  Serial.print(P,3);  
-  Serial.println("W");
-//  oled();             //mostrar en display
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_t0_13_tf);
-  u8g2.setCursor(2, 10);
-  u8g2.print("Ip: ");
-  u8g2.print(Ip,3);
-  u8g2.print("A");
-
-  u8g2.setCursor(2, 20);
-  u8g2.print("I-Rms: ");
-  u8g2.print(Irms,3);
-  u8g2.print("A");
-  
-  u8g2.setCursor(2, 30);
-  u8g2.print("Potencia: ");
-  u8g2.print(P,2);  
-  u8g2.print("W");
-  u8g2.sendBuffer();
-  delay(800);     
-}
-
-float get_corriente()
-{
-  float voltajeSensor;
-  float corriente=0;
-  long tiempo=millis();
-  float Imax=0;
-  float Imin=0;
-  while(millis()-tiempo<500)//realizamos mediciones durante 0.5 segundos
-  { 
-    voltajeSensor = analogRead(iPin) * (5.0 / 1023.0);//lectura del sensor
-    corriente=0.9*corriente+0.1*((voltajeSensor-2.527)/sensibilidad); //Ecuación  para obtener la corriente
-    if(corriente>Imax)Imax=corriente;
-    if(corriente<Imin)Imin=corriente;
-  }
-  return(((Imax-Imin)/2)-offset);
-}
-
-//void oled(){
-//  u8g2.clearBuffer();
-//  u8g2.setFont(u8g2_font_t0_13_tf);
-//  u8g2.setCursor(2, 10);
-//  u8g2.print("Ip: ");
-//  u8g2.print(Ip,3);
-//  u8g2.print("A");
-//
-//  u8g2.setCursor(2, 20);
-//  u8g2.print("I-Rms: ");
-//  u8g2.print(Irms,3);
-//  u8g2.print("A");
-//  
-//  u8g2.setCursor(2, 30);
-//  u8g2.print("Potencia: ");
-//  u8g2.print(P,3);  
-//  u8g2.print("W");
-//  u8g2.sendBuffer();
-//}
-//----------------------
 #include <Arduino.h>
-#include <U8g2lib.h>
 #include <Wire.h>
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 22, /* data=*/ 21, /* reset=*/ U8X8_PIN_NONE);
-//Descomentar según el sensor usado ya sea de 5, 20 0 30 Amperes
-//float sensibilidad=0.185; //Ajuste de la sensibilidad para sensor de 5 Amperes
-//float sensibilidad=0.100; //Ajuste de la sensibilidad para sensor de 20 Amperes
-float sensibilidad=0.66; //Ajuste de la sensibilidad para sensor de 30 Amperes
-float I=0.00;
-float ajuste = 0.05;//Sólo si es necesario, se usa para corregir posibles desvios en la lectura que se comparará con un amperímetro
-float iPin = 34; 
-void setup() {
-  Serial.begin(9600);//Velocidad del puerto serial
-  u8g2.begin(); //Inicializamos el display
-}
-void loop() { 
-  I=promedio_I(500);//Promedio de 500 muestras para mejorar la presición(llamamos a la función promedio_I()
-  Serial.print("Intencidad: ");
-  if(I>=0.01){//Filtro para eliminar pequeñas oscilasciones(ruido)
-  I= ((I*10)+ajuste),2;
-  Serial.println(I);//Imprime el valor de la corriente consumida
-oled();//Función para mostrar los datos obtenidos en el display  
-  delay(100); 
-  }else{
-  Serial.println("0.00");
-I = 0.00; 
-oled();//Función para mostrar en el display 0.00
-  delay(100);   
-  }
-}
-//Función para generar 500 muestas
-float promedio_I(int muestras_I)
-{
-  float sensorA0;
-  float intencidad=0;
-  for(int i=0;i<muestras_I;i++)
-  {
-    sensorA0 = analogRead(iPin) * (5.0 / 1023.0);//Leemos el sensor de corriente
-    intencidad=(0.8*intencidad)+(sensorA0-2.5)/sensibilidad; //Calculo para obtener el valor de la corriente
-  }
-  intencidad=intencidad/muestras_I;//dividimos por 500 
-  return(intencidad);
-}
-//Función para mostrar datos en el display oled
-void oled(){
-  u8g2.clearBuffer(); // Borra el contenido anterior del buffer de pantalla
-  u8g2.setFont(u8g2_font_t0_13_tf); // Selecciona una fuente grande
-  u8g2.setCursor(0, 40); // Establece la posición de la posición del texto
-  u8g2.print("I:");//Se muestra I: en el display
-  u8g2.print(I);//Se muestra el valor de corriente obtenido
-  u8g2.setCursor(60, 40); // Establece la posición de la posición del texto
-  u8g2.print("A");//Se muestra A en el display
-  u8g2.sendBuffer(); // Envia el contenido del buffer a la pantalla   
-  }
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=22*/ 13, /* data=21*/ 18, /* reset=*/ U8X8_PIN_NONE); //esp32
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //ardu
 
-////////// velocidad lineal 31/05/2024+corriente ok
-
-float sensibilidad=0.105;                       //sensibilidad en V/A para nuestro sensor 0.105
+float Ip=0;
+float sensibilidad=0.105;                       //sensibilidad en V/A para sensor (0.105)  5A 0.185/20A 0.100/30A 0.66
 float offset=0.136;                             // Equivale a la amplitud del ruido 0v 0a ---0.165  9v 0.04a ---0.186
                                                 // calibracion base 0.176
 int encoder = 2;                //Pin 2, donde se conecta el encoder       
@@ -196,6 +20,7 @@ static volatile unsigned long debounce = 0; //Tiempo del rebote.
       
 void setup() {
   Serial.begin(9600);
+  u8g2.begin();                 //Inicializamos el display
   pinMode(encoder, INPUT); 
   attachInterrupt(digitalPinToInterrupt(encoder), inter, RISING);  
   pulsos  = 0;
@@ -219,7 +44,7 @@ void loop() {
       Serial.println(vel,2); 
       pulsos  = 0;                                            //Inicializamos los pulsos.
       interrupts();                                           //Reiniciamos la interrupción
-  float Ip=get_corriente();                                   //obtenemos la corriente pico
+  Ip=get_corriente();                                         //obtenemos la corriente pico
   Serial.print("Ip: ");                                       //Visualizar resultados
   Serial.print(Ip,3);
   Serial.println("A");
@@ -230,6 +55,7 @@ void loop() {
 //  Serial.print("A, Potencia: ");
 //  Serial.print(P,3);  
 //  Serial.println("W");
+    oled();                                   //Función para mostrar los datos
   }     
 }
 
@@ -237,13 +63,21 @@ float get_corriente() {     //-------------------------------Funcion leo corrien
   float voltajesensor;
   float corriente=0;
   long tiempo=millis();
-  float Imax=0;
-  float Imin=0;
+  float Imax=0; 
+  float Imaxt=0;
+  float Imin=0; 
+  float Imint=0;
   while(millis()-tiempo<500)  {                  //realizamos mediciones durante 0.5 segundos
+    for(int i=0;i<500;i++); {                    //se promedian 500 mediciones
     voltajesensor = analogRead(A0) * (5.0 / 1023.0);                          //lectura del sensor
     corriente=0.9*corriente+0.1*((voltajesensor-2.527)/sensibilidad);         //Ecuación  para obtener la corriente
-    if(corriente>Imax)Imax=corriente;
-    if(corriente<Imin)Imin=corriente;
+      if(corriente>Imax){Imax=corriente;
+      Imaxt+=Imax;}
+      if(corriente<Imin){Imin=corriente;
+      Imint+=Imin;}
+    }
+    Imaxt/=500;
+    Imint/=500;
   }
   return(((Imax-Imin)/2)-offset);
 }                         //----------------------------------Fin leo corriente
@@ -253,4 +87,22 @@ void inter(){  //------------------------------------------Funcion interrupcion
   //comprueba encoder tiempo es superior a 1000 microsegundos
   debounce = micros(); // Almacena el tiempo para comprobar de rebote
   pulsos ++;}  
+}             //------------------------------------------Fin interrupcion
+
+void oled(){  //------------------------------------------Funcion diaplay
+  u8g2.clearBuffer();                           // Borra el contenido anterior del buffer de pantalla
+  u8g2.setFont( u8g2_font_6x13_tf/*u8g2_font_t0_11_mn*/);             // Selecciona fuente
+  u8g2.setCursor(2, 10);                        // Establece la posición de la posición del texto
+  u8g2.print("I:");                             //Se muestra I
+  u8g2.setCursor(70, 10); u8g2.print(Ip);                               //Se muestra el valor de corriente obtenido
+    u8g2.setCursor(2, 20);                       //Posición de texto
+    u8g2.print("Seconds ");
+    u8g2.setCursor(70, 20); u8g2.print(millis()/1000);
+    u8g2.setCursor(2, 40);
+    u8g2.print("RPM ");
+    u8g2.setCursor(70, 40); u8g2.print(rpm);
+    u8g2.setCursor(2, 50);
+    u8g2.print("Vel[Km/h]");
+    u8g2.setCursor(70, 50); u8g2.print(vel,2);
+  u8g2.sendBuffer();                            //Envia contenido   
 }             //------------------------------------------Fin interrupcion
